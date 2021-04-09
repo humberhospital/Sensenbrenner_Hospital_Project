114e3547493f96a0e6449431e237f65cb58c75d4
Luis Botello <botello468@gmail.com>
2021-04-08 21:24:46 -0400

    Added Luis data controllers

---
+++ Controllers/CareerDataController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SensenbrennerHospital.Models;
+
+namespace SensenbrennerHospital.Controllers
+{
+    public class CareerDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/CareerData
+        public IQueryable<Career> GetCareers()
+        {
+            return db.Careers;
+        }
+
+        // GET: api/CareerData/5
+        [ResponseType(typeof(Career))]
+        public IHttpActionResult GetCareer(int id)
+        {
+            Career career = db.Careers.Find(id);
+            if (career == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(career);
+        }
+
+        // PUT: api/CareerData/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCareer(int id, Career career)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != career.CareerID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(career).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CareerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CareerData
+        [ResponseType(typeof(Career))]
+        public IHttpActionResult PostCareer(Career career)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Careers.Add(career);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = career.CareerID }, career);
+        }
+
+        // DELETE: api/CareerData/5
+        [ResponseType(typeof(Career))]
+        public IHttpActionResult DeleteCareer(int id)
+        {
+            Career career = db.Careers.Find(id);
+            if (career == null)
+            {
+                return NotFound();
+            }
+
+            db.Careers.Remove(career);
+            db.SaveChanges();
+
+            return Ok(career);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CareerExists(int id)
+        {
+            return db.Careers.Count(e => e.CareerID == id) > 0;
+        }
+    }
+}
