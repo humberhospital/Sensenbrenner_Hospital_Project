bf713d75290da736fa4cfed59aa939e7172f2a41
Luis Botello <botello468@gmail.com>
2021-04-08 22:30:26 -0400

    Added correct namespace

--- Models/ViewModels/UpdateUser.cs
+++ Models/ViewModels/UpdateUser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace SensenbrennerHospital.Models
+namespace SensenbrennerHospital.Models.ViewModels
 {
     public class UpdateUser
     {
