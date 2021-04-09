74e0485f27ee672db8e1a3d8a2891ae7f2cceda5
Luis Botello <botello468@gmail.com>
2021-04-09 14:43:36 -0400

    Finished Career controller

--- Controllers/CareerController.cs
+++ Controllers/CareerController.cs
@@ -1,23 +1,65 @@
-﻿using System;
+﻿using SensenbrennerHospital.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace SensenbrennerHospital.Controllers
 {
     public class CareerController : Controller
     {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+        static CareerController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44375/api/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
         // GET: Career
         public ActionResult Index()
         {
-            return View();
+            string url = "CareerData/ListCareers";
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                IEnumerable<Career> CareerList = httpResponse.Content.ReadAsAsync<IEnumerable<Career>>().Result;
+                return View(CareerList);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // GET: Career/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            string url = "CareerData/GetCareer/" + id;
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                Career selectedCareer = new Career();
+                selectedCareer = httpResponse.Content.ReadAsAsync<Career>().Result;
+
+                return View(selectedCareer);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // GET: Career/Create
@@ -28,61 +70,101 @@ namespace SensenbrennerHospital.Controllers
 
         // POST: Career/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Career NewCareer)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            string url = "CareerData/AddCareer";
 
-                return RedirectToAction("Index");
-            }
-            catch
+            HttpContent content = new StringContent(jss.Serialize(NewCareer));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
+            Debug.WriteLine(jss.Serialize(NewCareer));
+
+            if (httpResponse.IsSuccessStatusCode)
             {
-                return View();
+                int CareerID = httpResponse.Content.ReadAsAsync<int>().Result;
+
+                return RedirectToAction("Details", new { id = CareerID });
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
         // GET: Career/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            string url = "CareerData/GetCareer/" + id;
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                Career selectedCareer = new Career();
+                selectedCareer = httpResponse.Content.ReadAsAsync<Career>().Result;
+
+                return View(selectedCareer);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // POST: Career/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Career selectedCareer)
         {
-            try
-            {
-                // TODO: Add update logic here
+            string url = "CareerData/UpdateCareer/" + id;
+            HttpContent content = new StringContent(jss.Serialize(selectedCareer));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (httpResponse.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("Details", new { id = id });
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
         // GET: Career/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult DeleteConfirm(int id)
         {
-            return View();
+            string url = "CareerData/GetCareer/" + id;
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                Career selectedCareer = new Career();
+                selectedCareer = httpResponse.Content.ReadAsAsync<Career>().Result;
+
+                return View(selectedCareer);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // POST: Career/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id)
         {
-            try
-            {
-                // TODO: Add delete logic here
+            string url = "CareerData/DeleteCareer/" + id;
+            HttpContent content = new StringContent("");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
 
+            if (httpResponse.IsSuccessStatusCode)
+            {
                 return RedirectToAction("Index");
             }
-            catch
+            else
             {
-                return View();
+                return RedirectToAction("Error");
             }
         }
     }
