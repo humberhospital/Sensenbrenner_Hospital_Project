9193461836d06ec013d08f0f34c6924f74b064b8
Luis Botello <botello468@gmail.com>
2021-04-09 14:26:42 -0400

    Finished appointment booking controller

--- Controllers/AppointmentBookingController.cs
+++ Controllers/AppointmentBookingController.cs
@@ -1,23 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using SensenbrennerHospital.Models;
 
 namespace SensenbrennerHospital.Controllers
 {
     public class AppointmentBookingController : Controller
     {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+        static AppointmentBookingController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44375/api/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // Homepage for this table shows all the bookings, this can be adjusted later
         // GET: AppointmentBooking
         public ActionResult Index()
         {
-            return View();
+            string url = "AppointmentBookingData/ListAppointmentBookings";
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                IEnumerable<AppointmentBooking> BookingList = httpResponse.Content.ReadAsAsync<IEnumerable<AppointmentBooking>>().Result;
+                return View(BookingList);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
+        // Shows detail for a certain entry
         // GET: AppointmentBooking/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            string url = "AppointmentBookingData/GetAppointmentBooking/" + id;
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                AppointmentBooking selectedBooking = new AppointmentBooking();
+                selectedBooking = httpResponse.Content.ReadAsAsync<AppointmentBooking>().Result;
+
+                return View(selectedBooking);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // GET: AppointmentBooking/Create
@@ -28,61 +73,102 @@ namespace SensenbrennerHospital.Controllers
 
         // POST: AppointmentBooking/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(AppointmentBooking NewAppointmentBooking)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            string url = "AppointmentBookingData/AddAppointmentBooking";
 
-                return RedirectToAction("Index");
-            }
-            catch
+            HttpContent content = new StringContent(jss.Serialize(NewAppointmentBooking));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
+            Debug.WriteLine(jss.Serialize(NewAppointmentBooking));
+
+            if (httpResponse.IsSuccessStatusCode)
             {
-                return View();
+                int BookingID = httpResponse.Content.ReadAsAsync<int>().Result;
+
+                return RedirectToAction("Details", new { id = BookingID });
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
         // GET: AppointmentBooking/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            string url = "AppointmentBookingData/GetAppointmentBooking/" + id;
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                AppointmentBooking selectedBooking = new AppointmentBooking();
+                selectedBooking = httpResponse.Content.ReadAsAsync<AppointmentBooking>().Result;
+
+                return View(selectedBooking);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // POST: AppointmentBooking/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, AppointmentBooking selectedBooking)
         {
-            try
-            {
-                // TODO: Add update logic here
+            string url = "AppointmentBookingData/UpdateAppointmentBooking/" + id;
+            HttpContent content = new StringContent(jss.Serialize(selectedBooking));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (httpResponse.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("Details", new { id = id });
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
         // GET: AppointmentBooking/Delete/5
-        public ActionResult Delete(int id)
+        [HttpGet]
+        public ActionResult DeleteConfirm(int id)
         {
-            return View();
+            string url = "AppointmentBookingData/GetAppointmentBooking/" + id;
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                AppointmentBooking selectedBooking = new AppointmentBooking();
+                selectedBooking = httpResponse.Content.ReadAsAsync<AppointmentBooking>().Result;
+
+                return View(selectedBooking);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // POST: AppointmentBooking/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id)
         {
-            try
-            {
-                // TODO: Add delete logic here
+            string url = "AppointmentBookingData/DeleteAppointmentBooking/" + id;
+            HttpContent content = new StringContent("");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (httpResponse.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
     }
