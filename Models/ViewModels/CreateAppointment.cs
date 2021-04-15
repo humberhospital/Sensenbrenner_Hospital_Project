855563ff40d0037b835860604ee2498f856389c0
Luis Botello <botello468@gmail.com>
2021-04-15 16:42:51 -0400

    Added viewmodel for create appointment page

---
+++ Models/ViewModels/CreateAppointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SensenbrennerHospital.Models.ViewModels
+{
+    public class CreateAppointment
+    {
+        //Conditionally render update/delete links if admin
+        public bool isAdmin { get; set; }
+
+        //conditionally render 'add donation' if user
+        public bool isUser { get; set; }
+
+        public AppointmentBooking appointmentBooking { get; set; }
+        public IEnumerable<DoctorDTO> doctorDTOs { get; set; }
+
+    }
+}
