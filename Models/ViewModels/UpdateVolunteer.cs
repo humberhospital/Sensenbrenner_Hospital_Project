8bf3d7fce5705111cc107c473ccd2ac95e2aaf59
elle robertson <elea.roberts@hotmail.com>
2021-04-17 19:12:57 -0400

    Volunteer test

---
+++ Models/ViewModels/UpdateVolunteer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SensenbrennerHospital.Models.ViewModels
+{
+    //The View Model required to update a volunteer
+    public class UpdateVolunteer
+    {
+        //Information about the volunteer
+        public VolunteerDto volunteer { get; set; }
+        //Needed for a dropdownlist which presents the choice of departments for the coluntter to be part of
+        public IEnumerable<DepartmentDto> alldepartments { get; set; }
+    }
+}
