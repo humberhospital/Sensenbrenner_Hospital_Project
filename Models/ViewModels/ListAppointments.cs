1981e85983553a75ab02b50c0c1a4802a206f4c0
Luis Botello <botello468@gmail.com>
2021-04-23 09:54:22 -0400

    Added appointment booking view model

---
+++ Models/ViewModels/ListAppointments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SensenbrennerHospital.Models.ViewModels
+{
+    public class ListAppointments
+    {
+        public bool isadmin { get; set; }
+        public AppointmentBookingDto appointment { get; set; }
+        public IEnumerable<CategoryDto> category { get; set; }
+    }
+}
